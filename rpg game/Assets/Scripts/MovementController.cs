0620d6ff859fb40603106624b501d81135a9bf59
Francesc Folch Company <frafolcm@gmail.com>
2021-03-27 20:14:34 +0100

    prefabs
    Created prefabs for enemy and player with animations.
    
    Implemented first version for player movement

---
+++ rpg game/Assets/Scripts/MovementController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementController : MonoBehaviour
+{
+    public float movementSpeed = 3.0f;
+    Vector2 movement = new Vector2();
+    Rigidbody2D rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate() {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        movement = Vector2.ClampMagnitude(movement, 1.0f);
+        rb.velocity = movement * movementSpeed;
+
+    }
+}
