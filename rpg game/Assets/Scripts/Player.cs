355f697abf137b3b7632df1ea653eed38226faa5
Francesc Folch Company <frafolcm@gmail.com>
2021-04-04 01:09:56 +0200

    sprites and movement
    Imported player sprites and atlases.
    
    Implemented basic grid movement

---
+++ rpg game/Assets/Scripts/Player.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : Character
+{
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.CompareTag("CanBeCollected")){
+            other.gameObject.SetActive(false);
+        }
+    }
+}
